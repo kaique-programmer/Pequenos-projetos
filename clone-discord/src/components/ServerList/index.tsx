caf8adaeae12f8e398f4bc562f29c5bdce16bf60
kaique oliveira santos <kaiquevicio@gmail.com>
2020-09-23 19:03:57 -0300

    Estrutura da coluna server-list, pronta.

---
+++ clone-discord/src/components/ServerList/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import ServerButton from "../ServerButton";
+
+import { Container, Separator } from './styles';
+
+const ServerList: React.FC = () => {
+  return (
+    <Container>
+      <ServerButton isHome />
+
+      <Separator />
+
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton mentions={1} />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton mentions={10} />
+      <ServerButton />
+      <ServerButton />
+    </Container>
+  );
+};
+
+export default ServerList;
