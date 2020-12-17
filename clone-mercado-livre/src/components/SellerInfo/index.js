0b9cc92796243341301d1c558cd7634e75c69f38
kaique oliveira santos <kaiquevicio@gmail.com>
2020-12-17 18:48:50 -0300

    Estrutura e estilização das informações do vendedor, finalizada.

--- clone-mercado-livre/src/components/SellerInfo/index.js
+++ clone-mercado-livre/src/components/SellerInfo/index.js
@@ -1,11 +1,64 @@
 import React from 'react';
 
-import { Container } from './styles';
+import {
+  Container,
+  Title,
+  LocationCard,
+  LocationIcon,
+  ReputationCard,
+  ReputationThermometer,
+  ReputationRow,
+  SupportIcon,
+  ClockIcon,
+  More,
+} from './styles';
 
 export default function Seller() {
   return (
     <Container>
-      <h1>Seller</h1>
+      <Title>Informações sobre o vendedor</Title>
+
+      <LocationCard>
+        <LocationIcon />
+
+        <div>
+          <p>Localização</p>
+          <strong>São Paulo, SP</strong>
+        </div>
+      </LocationCard>
+
+      <ReputationCard>
+        <ReputationThermometer>
+          <li />
+          <li />
+          <li />
+          <li />
+          <li className="active" />
+        </ReputationThermometer>
+
+        <ReputationRow>
+          <div>
+            <strong>555</strong>
+            <span>vendas nos últimos 4 meses</span>
+          </div>
+
+          <div>
+            <strong>
+              <SupportIcon />
+            </strong>
+            <span>Presta um bom atendimento</span>
+          </div>
+
+          <div>
+            <strong>
+              <ClockIcon />
+            </strong>
+            <span>vendas nos últimos 4 meses</span>
+          </div>
+        </ReputationRow>
+      </ReputationCard>
+
+      <More href="#">Ver mais dados do vendedor</More>
     </Container>
   );
 }
