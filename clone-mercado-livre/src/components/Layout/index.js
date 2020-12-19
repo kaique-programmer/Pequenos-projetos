8a3d6437f4afcd8b1fe10b2ceff9ceb7eaa3725d
kaique oliveira santos <kaiquevicio@gmail.com>
2020-12-19 01:43:02 -0300

    Componentes: Header, Footer, Layout, Product, ProductAction e SelleInfo, implementados.

--- clone-mercado-livre/src/components/Layout/index.js
+++ clone-mercado-livre/src/components/Layout/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
 import Product from '../Product';
+import Header from '../Header';
+import Footer from '../Footer';
 
 import { Container, Wrapper } from './styles';
 
 export default function Layout() {
   return (
     <Container>
-      {/* <Header /> */}
+      <Header />
 
       <Wrapper>
         <Product />
       </Wrapper>
 
-      {/* <Footer /> */}
+      <Footer />
     </Container>
   );
 }
