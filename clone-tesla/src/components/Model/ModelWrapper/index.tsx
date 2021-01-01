6dcdfb8c2f99e73a18858f0057584150aca960e9
kaique oliveira santos <kaiquevicio@gmail.com>
2021-01-01 16:32:27 -0300

    Registro de modelos na context API

--- clone-tesla/src/components/Model/ModelWrapper/index.tsx
+++ clone-tesla/src/components/Model/ModelWrapper/index.tsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 
-import { Container } from './styles';
+import ModelsContext, { CarModel } from '../ModelsContext';
+
+import { Container, OverlaysRoot, ModelOverlay } from './styles';
 
 const ModelWrapper: React.FC = ({ children }) => {
-  return <Container>{children}</Container>;
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  const [registeredModels, setRegisteredModels] = useState<CarModel[]>([]);
+
+  const registerModel = useCallback((model: CarModel) => {
+    setRegisteredModels((state) => [...state, model]);
+  }, []);
+
+  const unregisterModel = useCallback((modelName: string) => {
+    setRegisteredModels((state) =>
+      state.filter((model) => model.modelName !== modelName)
+    );
+  }, []);
+
+  const getModelByName = useCallback(
+    (modelName: string) => {
+      return (
+        registeredModels.find((item) => item.modelsName === modelName) || null
+      );
+    },
+    [registeredModels]
+  );
+
+  return (
+    <ModelsContext.Provider
+      value={{
+        wrapperRef,
+        registeredModels,
+        registerModel,
+        unregisterModel,
+        getModelByName,
+      }}
+    >
+      <Container ref={wrapperRef}>
+        <OverlaysRoot>
+          {registeredModels.map((item) => (
+            <ModelOverlay key={item.modelName}>{item.overlayNode}</ModelOverlay>
+          ))}
+        </OverlaysRoot>
+
+        {children}
+      </Container>
+    </ModelsContext.Provider>
+  );
 };
 
 export default ModelWrapper;
