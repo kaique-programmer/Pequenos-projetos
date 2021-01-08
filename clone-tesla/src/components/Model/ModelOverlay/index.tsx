ed9599d81280d4c8fcd76e6191be1d87b56c87cf
kaique oliveira santos <kaiquevicio@gmail.com>
2021-01-08 13:01:05 -0300

    Criação de hook de scroll

--- clone-tesla/src/components/Model/ModelOverlay/index.tsx
+++ clone-tesla/src/components/Model/ModelOverlay/index.tsx
@@ -1,8 +1,12 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import useWrapperScroll from '../useWrapperScroll';
 
 import { Container } from './styles';
 
 const ModelOverlay: React.FC = ({ children }) => {
+  useWrapperScroll();
+
   return <Container>{children}</Container>;
 };
 
