4d103686dccc0aeff6b8d0beed59ec04afe58e77
kaique oliveira santos <kaiquevicio@gmail.com>
2021-11-04 10:34:50 -0300

    delete site final

--- Apredizagem básica de HTML/SITE FINAL/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(!@$con = mysql_connect('localhost','root','')){
-	echo "Erro na base de dados";
-}
-if(!mysql_select_db('bd_veiculos', $con)){
-	echo "Erro ao selecionar base de dados!";
-}
-mysql_query("SET NAMES utf8");
-?>
